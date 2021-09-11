aaf901590da0e57a4a1aa35396203d8405751bbb
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-09-12 00:38:43 +0100

    Create MyGdxGame.java

---
+++ gdx-game/MyGdxGame.java
@@ -0,0 +1,14 @@
+package com.jouhou;
+
+import com.aminedev.run.MenuScreen;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public class MyGdxGame extends Game {
+    
+    @Override
+    public void create() {
+		setScreen(new MenuScreen());
+    }
+}
+ 
