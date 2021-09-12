f4bd3c0b289b5b5aa56f651b94451ec4bd78bf0c
1337xp <1337xp101@gmail.com>
2021-09-13 00:45:35 +0200

    Updated code

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import random
 import sys
 import pygame
 import math
-from pygame.locals import Color, KEYUP, K_ESCAPE, K_LEFT,K_RIGHT,K_DOWN,KEYDOWN
+from pygame.locals import Color
 from pygame.math import Vector2
 import numpy as np
 class spritesheet(object):
@@ -92,6 +91,8 @@ def length(x, y):
 def norm(x, y):
     _len    = length(x, y)
     return x / _len, y / _len
+
+lol22 = False
 class Player(pygame.sprite.Sprite):
     """
     Spawn a player
@@ -126,7 +127,12 @@ class Player(pygame.sprite.Sprite):
                 self.image = self.images[0].next()
             self.rect.x = self.rect.x + self.movex
             self.rect.y = self.rect.y + self.movey
-
+            if self.rect.x > 400:
+                self.rect.x = 400
+            if self.rect.y < 0:
+                self.rect.y = 0
+            if self.rect.x < 0:
+                self.rect.x = 0
             # moving left
             if self.movex < 0:
                 self.frame += 1
@@ -141,46 +147,79 @@ class Boss(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("images/YuukaKazamiBoss.png")
         self.rect = self.image.get_rect()
+        self.counter = 0
+    def update(self):
+        '''
+        enemy movement
+        '''
+        distance = 300
+        speed = 1.25
+        if self.counter >= 0 and self.counter <= distance:
+            self.rect.x += speed
+        elif self.counter >= distance and self.counter <= distance*2:
+            self.rect.x -= speed
+        else:
+            self.counter = 0
 
-class BossBullet(pygame.sprite.Sprite):
+        self.counter += 1
+
+class SuperBullet(pygame.sprite.Sprite):
 
 
-    def __init__(self,playerx,playery,spin):
+    def __init__(self,playerx=0,playery=0,spin=0,gotoPlayer=False):
+        self.gotoplayer = gotoPlayer
         pygame.sprite.Sprite.__init__(self,)
         self.spin = spin
         self.image = pygame.image.load("images/bullet.png")
         self.image.set_colorkey((120,120,120))
         self.rect = self.image.get_rect()
-        self.rect.center = (90,90)
+        self.rect.center = (0,0)
         self.angle = 120
         self.counter = 0
         self.rect.x = playerx
         self.rect.y = playery
         self.pos = Vector2(self.rect.center)
         self.spin1 = 0
+
     def update(self):
-        print(self.spin)
-        self.velocity = Vector2(0.25, 0).rotate(self.spin*100-90) * 5
-        self.pos += self.velocity
-        self.rect.center = self.pos
-        if (self.rect.right > 1000 or self.rect.left < 0
-                or self.rect.bottom > 1000 or self.rect.top < 0):
-            self.kill()
-class Bullet(pygame.sprite.Sprite):
-    """ This class represents the bullet . """
+        if lol22 is False:
+            self.velocity = Vector2(0.25, 0).rotate(self.spin*100-90) * 5
+            self.pos += self.velocity
+            self.rect.center = self.pos
+            if (self.rect.right > 400 or self.rect.left < 0
+                    or self.rect.bottom > 600 or self.rect.top < 0):
+                self.kill()
 
-    def __init__(self):
-        # Call the parent class (Sprite) constructor
-        super().__init__()
+        else:
+                (dx, dy) = ((player.rect.x - self.rect.x) / math.sqrt(
+                    (player.rect.x - self.rect.x) ** 2 + (player.rect.y - self.rect.y) ** 2),
+                            (player.rect.y - self.rect.y) / math.sqrt(
+                                (player.rect.x - self.rect.x) ** 2 + (player.rect.y - self.rect.y) ** 2))
+                self.rect.x = self.rect.x + dx * 3
+                self.rect.y = self.rect.y + dy * 3
+                collide = self.rect.collidepoint((player.rect.x,player.rect.y))
+                if collide:
+                    self.kill()
 
-        self.image = pygame.image.load('images/bullet.png')
 
+
+class EnemyLaser(pygame.sprite.Sprite):
+    def __init__(self,x,time=1):
+        pygame.sprite.Sprite.__init__(self,)
+        self.image = pygame.Surface((20, 600))
+        self.image.fill((0,22,122))
         self.rect = self.image.get_rect()
-
+        self.rect.x = x
+        self.time = time
+        self.counter = 0
     def update(self):
-        """ Move the bullet. """
-        self.rect.y -= 3
-surface = pygame.display.set_mode((1000,1000))
+        self.counter += 1
+        if self.counter >= 300:
+            self.kill()
+            self.counter = 0
+
+
+surface = pygame.display.set_mode((800,600))
 FPS = 120
 frames = FPS / 12
 all_sprites_list = pygame.sprite.Group()
@@ -194,24 +233,49 @@ player = Player()
 player.rect.x = 2  # go to x
 player.rect.y = 2  # go to y
 player_list = pygame.sprite.Group()
+boss_list = pygame.sprite.Group()
 player_list.add(player)
 steps = 1
+yuukakazami = Boss()
 black = Color('black')
 clock = pygame.time.Clock()
 image = SpriteStripAnim('images/links.gif', (0, 0, 32, 50), 8, 1, True, frames=frames)
 bulletcounter = 0
 dt = 0
 shootTime = 0
+shootTime1 = 0
+pygame.mixer.init()
+pygame.mixer.music.load('yuuka.wav')
+pygame.mixer.music.play(-1)
 shooting = 0
 n = 0
+omegapower = 1
+firerate = 10
 while True:
+    pygame.sprite.spritecollide(Boss(), bullet_list, True)
+    shootTime1 +=1
+    if shootTime1 >= 22:
+        #bullet_list.add(SuperBullet(playerx=yuukakazami.rect[0],playery=yuukakazami.rect[1], spin=180))
+        shootTime1 = 0
+    if pygame.time.get_ticks() in range(2000,2010):
+        print("lol")
+        boss_list.add(yuukakazami)
+    if bullet_list.sprites() == []:
+        lol22 = False
+
+
     current_time = pygame.time.get_ticks()
     shootTime += 1
     dt += clock.tick(120) / 1000
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == ord('z'):
+                lol22 = False
+                bullet_list.remove(SuperBullet())
                 shooting = 1
+            if event.key == ord('x'):
+               lol22 = True
+               bullet_list.add(EnemyLaser(x=player.rect.x, time=1))
             if event.key == pygame.K_LEFT or event.key == ord('a'):
                 player.control(-steps, 0)
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
@@ -222,8 +286,10 @@ while True:
                 player.control(0, steps)
 
         if event.type == pygame.KEYUP:
+
             if event.key == ord('z'):
                 shooting = 0
+                n = 0
             if event.key == pygame.K_LEFT or event.key == ord('a'):
                 player.control(steps, 0)
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
@@ -238,23 +304,33 @@ while True:
                 pygame.quit()
                 sys.exit()
     shootTime += 1
-    if shootTime >= 20 and shooting == 1:
+    if omegapower == 1:
+        firerate = 5
+    else:
+        firerate = 20
+    if shootTime >= firerate and shooting == 1:
         n += 1
-        if n == 60:
+        if n == 40:
             n = 0
         Fs = 8000
-        f = 5
-        sample = 8000
+        f = 200
+        f1 = 200
+        sample = 1000
         x = np.arange(sample)
         y = np.sin(2 * np.pi * f * x / Fs)
+        y1 = -np.sin(2 * np.pi * f1 * x / Fs)
+        y2 = -np.sin(2 * np.pi * f1-200 * x / Fs)
         # Fire a bullet if the user clicks the mouse button
-        bullet = Bullet()
         # Set the bullet so it is where the player is
-        bullet.rect.x = player.rect.x + 10
-        bullet.rect.y = player.rect.y
         # Add the bullet to the listsd
-        bullet_list.add(bullet)
-        bullet_list.add(BossBullet(player.rect.x,player.rect.y,spin=x[n]))
+        bullet_list.add(SuperBullet(player.rect.x,player.rect.y,spin=y1[n]))
+        bullet_list.add(SuperBullet(player.rect.x, player.rect.y, 0))
+
+
+        if omegapower == 1:
+            bullet_list.add(SuperBullet(player.rect.x,player.rect.y,spin=y[n]))
+            bullet_list.add(SuperBullet(player.rect.x, player.rect.y, spin=y2[n]))
+            bullet_list.add(SuperBullet(player.rect.x, player.rect.y, spin=-y2[n]))
         shootTime = 0
     for bullet in bullet_list:
 
@@ -267,5 +343,8 @@ while True:
     player.update()
     bullet_list.update()
     player_list.draw(surface)
+    boss_list.update()
+    boss_list.draw(surface)
+
     bullet_list.draw(surface)
     pygame.display.flip()
